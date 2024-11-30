c391b1f7373e7649a71f69024585f8c50051ac3a
Jon <mrahimzod@gmail.com>
2024-12-01 00:27:01 +0500

    .net8.0

--- obj/Debug/net8.0/Indotalent.AssemblyInfo.cs
+++ obj/Debug/net8.0/Indotalent.AssemblyInfo.cs
@@ -11,12 +11,11 @@
 using System;
 using System.Reflection;
 
-[assembly: Microsoft.AspNetCore.Identity.UI.UIFrameworkAttribute("Bootstrap5")]
 [assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("aspnet-Indotalent-b92680c3-3300-4a79-b996-d570e9f89f5d")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("Indotalent")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+828d60ab41c19882df96cc51093c70a96f60eb5a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6557ea71b45c000b91c3dbd3a00260a6a2f9057")]
 [assembly: System.Reflection.AssemblyProductAttribute("Indotalent")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Indotalent")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
